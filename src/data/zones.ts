1090ecaacf9982cb2dd976bc86ac1a7fc2533a46
andersseen <andriipap01@gmail.com>
2025-11-02 18:08:40 +0100

    Update reviews

---
+++ src/data/zones.ts
@@ -0,0 +1,40 @@
+export interface Zone {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export const zones: Zone[] = [
+  { id: 'o14', name: 'O-14', image: '00.png' },
+  { id: 'p14', name: 'P-14', image: '01.png' },
+  { id: 'q14', name: 'Q-14', image: '02.png' },
+  { id: 'r14', name: 'R-14', image: '03.png' },
+  { id: 'o15', name: 'O-15', image: '10.png' },
+  { id: 'p15', name: 'P-15', image: '11.png' },
+  { id: 'q15', name: 'Q-15', image: '12.png' },
+  { id: 'r15', name: 'R-15', image: '13.png' },
+  { id: 'o16', name: 'O-16', image: '20.png' },
+  { id: 'p16', name: 'P-16', image: '21.png' },
+  { id: 'q16', name: 'Q-16', image: '22.png' },
+  { id: 'r16', name: 'R-16', image: '23.png' },
+  { id: 'o17', name: 'O-17', image: '30.png' },
+  { id: 'p17', name: 'P-17', image: '31.png' },
+  { id: 'q17', name: 'Q-17', image: '32.png' },
+  { id: 'r17', name: 'R-17', image: '33.png' },
+  { id: 'o18', name: 'O-18', image: '40.png' },
+  { id: 'p18', name: 'P-18', image: '41.png' },
+  { id: 'q18', name: 'Q-18', image: '42.png' },
+  { id: 'r18', name: 'R-18', image: '43.png' },
+  { id: 'o19', name: 'O-19', image: '50.png' },
+  { id: 'p19', name: 'P-19', image: '51.png' },
+  { id: 'q19', name: 'Q-19', image: '52.png' },
+  { id: 'r19', name: 'R-19', image: '53.png' },
+  { id: 'o20', name: 'O-20', image: '60.png' },
+  { id: 'p20', name: 'P-20', image: '61.png' },
+  { id: 'q20', name: 'Q-20', image: '62.png' },
+  { id: 'r20', name: 'R-20', image: '63.png' },
+  { id: 'o21', name: 'O-21', image: '70.png' },
+  { id: 'p21', name: 'P-21', image: '71.png' },
+  { id: 'q21', name: 'Q-21', image: '72.png' },
+  { id: 'r21', name: 'R-21', image: '73.png' },
+];
