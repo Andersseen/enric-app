d69e155a15b7a44a1ba206588c537e0871d77d5c
andersseen <andriipap01@gmail.com>
2025-11-15 15:48:44 +0100

    Upodate Form

--- src/components/form.ts
+++ src/components/form.ts
@@ -8,12 +8,15 @@ import {
   IonSegment,
   IonSegmentButton,
   IonTextarea,
+  IonButton,
 } from '@ionic/angular/standalone';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import StoreService from '@service/state';
 
 @Component({
   selector: 'app-form',
   imports: [
+    ReactiveFormsModule,
     IonInput,
     IonItem,
     IonSelect,
@@ -22,75 +25,138 @@ import StoreService from '@service/state';
     IonSegment,
     IonSegmentButton,
     IonTextarea,
+    IonButton,
   ],
   template: `
-    <ion-list class="flex flex-col gap-4">
-      <ion-item>
-        <ion-input label="Area" [value]="zone().id" [disabled]="true" />
-      </ion-item>
-      <ion-item>
-        <ion-input label="Especie" [value]="bird().commonName" [disabled]="true" />
-      </ion-item>
-      <ion-item>
-        <ion-input label="Cuantas" type="number" placeholder="00" class="text-end" />
-      </ion-item>
-      <ion-item>
-        <ion-select interface="action-sheet" label="Actitud">
-          <ion-select-option>Posado</ion-select-option>
-          <ion-select-option>Volando</ion-select-option>
-          <ion-select-option>Cruzando</ion-select-option>
-          <ion-select-option>Alimentación</ion-select-option>
-          <ion-select-option>Atrapado</ion-select-option>
-        </ion-select>
-      </ion-item>
-      <ion-item>
-        <ion-segment value="Prevención">
-          <ion-segment-button value="Prevención"> Prevención </ion-segment-button>
-          <ion-segment-button value="Dispersión"> Dispersión </ion-segment-button>
-        </ion-segment>
-      </ion-item>
-      <ion-item>
-        <ion-select interface="action-sheet" label="Interacción operación">
-          <ion-select-option>Si</ion-select-option>
-          <ion-select-option>No</ion-select-option>
-        </ion-select>
-      </ion-item>
-      <ion-item>
-        <ion-select interface="action-sheet" label="Método empleado">
-          <ion-select-option>Claxon</ion-select-option>
-          <ion-select-option>Sonido</ion-select-option>
-          <ion-select-option>Fogueo</ion-select-option>
-          <ion-select-option>Laser</ion-select-option>
-          <ion-select-option>Vuelo dispersión halcón</ion-select-option>
-        </ion-select>
-      </ion-item>
-      <ion-item>
-        <ion-textarea label="Animal empleado" placeholder="Escribe algo..."></ion-textarea>
-      </ion-item>
-      <ion-item>
-        <ion-select interface="action-sheet" label="Eficacia">
-          <ion-select-option>Si</ion-select-option>
-          <ion-select-option>No</ion-select-option>
-        </ion-select>
-      </ion-item>
-      <ion-item>
-        <ion-input
-          label="Captura número individuo"
-          type="number"
-          placeholder="00"
-          class="text-end"
-        />
-      </ion-item>
-      <ion-item>
-        <ion-textarea label="Observaciones" placeholder="Escribe algo..."></ion-textarea>
-      </ion-item>
-    </ion-list>
+    <form>
+      <ion-list class="flex flex-col gap-4" [formGroup]="form">
+        <ion-item>
+          <ion-input label="Area" formControlName="zoneId" class="text-end" />
+        </ion-item>
+
+        <ion-item>
+          <ion-input label="Especie" formControlName="speciesId" class="text-end" />
+        </ion-item>
+
+        <ion-item>
+          <ion-input
+            label="Cuantas"
+            type="number"
+            formControlName="count"
+            placeholder="00"
+            class="text-end"
+          />
+        </ion-item>
+
+        <ion-item>
+          <ion-select interface="action-sheet" label="Actitud" formControlName="behavior">
+            <ion-select-option>Posado</ion-select-option>
+            <ion-select-option>Volando</ion-select-option>
+            <ion-select-option>Cruzando</ion-select-option>
+            <ion-select-option>Alimentación</ion-select-option>
+            <ion-select-option>Atrapado</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item>
+          <ion-segment formControlName="actionType">
+            <ion-segment-button value="Prevención">Prevención</ion-segment-button>
+            <ion-segment-button value="Dispersión">Dispersión</ion-segment-button>
+          </ion-segment>
+        </ion-item>
+
+        <ion-item>
+          <ion-select
+            interface="action-sheet"
+            label="Interacción operación"
+            formControlName="interaction"
+          >
+            <ion-select-option>Si</ion-select-option>
+            <ion-select-option>No</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item>
+          <ion-select interface="action-sheet" label="Método empleado" formControlName="method">
+            <ion-select-option>Claxon</ion-select-option>
+            <ion-select-option>Sonido</ion-select-option>
+            <ion-select-option>Fogueo</ion-select-option>
+            <ion-select-option>Laser</ion-select-option>
+            <ion-select-option>Vuelo dispersión halcón</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item>
+          <ion-textarea
+            label="Animal empleado"
+            placeholder="Escribe algo..."
+            formControlName="animal"
+            class="text-end"
+          ></ion-textarea>
+        </ion-item>
+
+        <ion-item>
+          <ion-select interface="action-sheet" label="Eficacia" formControlName="efficacy">
+            <ion-select-option>Si</ion-select-option>
+            <ion-select-option>No</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item>
+          <ion-input
+            label="Captura número individuo"
+            type="number"
+            formControlName="captured"
+            placeholder="00"
+            class="text-end"
+          />
+        </ion-item>
+
+        <ion-item>
+          <ion-textarea
+            label="Observaciones"
+            placeholder="Escribe algo..."
+            formControlName="notes"
+            class="text-end"
+          ></ion-textarea>
+        </ion-item>
+      </ion-list>
+      <ion-button (click)="save()" expand="block">Guardar</ion-button>
+    </form>
   `,
-  host: { class: 'block h-full w-full font-sans antialiased' },
 })
 export default class Form {
   #store = inject(StoreService);
 
   zone = computed(() => this.#store.step1Value());
   bird = computed(() => this.#store.step2Value());
+
+  form = inject(FormBuilder).group({
+    zoneId: [{ value: this.zone()?.id, disabled: true }],
+    speciesId: [{ value: this.bird()?.commonName, disabled: true }],
+
+    count: [0, [Validators.required, Validators.min(0)]],
+    behavior: [''],
+    actionType: ['Prevención'],
+    interaction: [''],
+    method: [''],
+    animal: [''],
+    efficacy: [''],
+    captured: [0, Validators.min(0)],
+    notes: [''],
+  });
+
+  save() {
+    console.log(this.form.value);
+
+    // if (this.form.invalid) {
+    //   this.form.markAllAsTouched();
+    //   return { ok: false, errors: ['Formulario inválido'] };
+    // }
+
+    // return {
+    //   ok: true,
+    //   payload: this.form.getRawValue(),
+    // };
+  }
 }
