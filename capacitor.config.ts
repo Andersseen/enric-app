9c8b663456f1cc4bad942dd29e5e24b336f19787
andersseen <andriipap01@gmail.com>
2026-01-18 14:54:39 +0100

    Add ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.enric.app',
+  appName: 'Enric App',
+  webDir: 'dist/enric-app/browser'
+};
+
+export default config;
