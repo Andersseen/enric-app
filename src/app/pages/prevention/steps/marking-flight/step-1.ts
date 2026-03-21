0be9e8309ad201037cf95f39b49b73505891071d
andersseen <andriipap01@gmail.com>
2026-03-21 14:23:28 +0100

    feat: Update flight step one template to enhance navigation and button functionality

--- src/app/pages/prevention/steps/marking-flight/step-1.ts
+++ src/app/pages/prevention/steps/marking-flight/step-1.ts
@@ -10,11 +10,15 @@ import { Zone } from '@data/zones';
   changeDetection: ChangeDetectionStrategy.OnPush,
 
   template: `
-    <app-prevention-step-page title="Vuelo de Marcaje - Zona">
+    <app-prevention-step-page
+      title="Vuelo de Marcaje - Zona"
+      [showNext]="true"
+      [canGoNext]="!!store.flightReviewZone()"
+      (nextClicked)="next()"
+    >
       <app-prevention-step-zone
         [selected]="store.flightReviewZone()"
         (select)="onSelect($event)"
-        (next)="next()"
       />
     </app-prevention-step-page>
   `,
